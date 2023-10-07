55bdb2c08b46a1f272af16a58000d7ce296e6044
hamkkong <akhenkitera@gmail.com>
2023-10-07 23:05:25 +0900

    fourth commit

--- src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
+++ src/test/java/moviebuddy/domain/JaxbMovieReaderTest.java
@@ -2,15 +2,22 @@ package moviebuddy.domain;
 
 import java.util.List;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
 public class JaxbMovieReaderTest {
 	
-	public static void main(String[] args) {
+	@Test 
+	void NotEmpty_LoaderMovies() {
+	// public static void main(String[] args) {
 		
 		// 객체 생성 
 		JaxbMovieReader movieReader = new JaxbMovieReader();
 		
 		List<Movie>movies = movieReader.loadMovies();
 		// movies.size() =>> XML문서에 등록된 영화 수와 동일한가?
-		MovieFinderTest.assertEquals(1375, movies.size());
+		Assertions.assertEquals(1375, movies.size());
+		//MovieFinderTest.assertEquals(1375, movies.size());
 	}
 }
